8f86c4860ce4560a30746f0a5b20fc219ba3624c
sawanya000 <sawanya000@gmail.com>
2021-04-07 11:01:27 +0700

    add start

--- index.js
+++ index.js
@@ -3,13 +3,34 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const requestIp = require("request-ip");
 
-app.use(requestIp.mw());
+// app.use(requestIp.mw());
+app.use(requestIp.mw({ attributeName: "myCustomAttributeName" }));
 
 app.use("/ip", function (req, res) {
   const ip = req.clientIp;
   const port = req.clientPort;
-//   res.send(ip+' '+req.connection.remotePort);
-    res.send(req.connection.remoteAddress+' '+req.connection.remotePort+' '+req.connection.localAddress+' '+req.connection.localPort);
+  //   res.send(ip+' '+req.connection.remotePort);
+  res.send(
+    req.connection.remoteAddress +
+      " " +
+      req.connection.remotePort +
+      " " +
+      req.connection.localAddress +
+      " " +
+      req.connection.localPort
+  );
+});
+
+app.use("/ip2", function (req, res) {
+  // use our custom attributeName that we registered in the middleware
+  var ip = req.myCustomAttributeName;
+  console.log(ip);
+
+  // https://nodejs.org/api/net.html#net_net_isip_input
+  var ipType = net.isIP(ip); // returns 0 for invalid, 4 for IPv4, and 6 for IPv6
+  res.end(
+    "Hello, your ip address is " + ip + " and is of type IPv" + ipType + "\n"
+  );
 });
 
 app.get("/", (req, res) => {
