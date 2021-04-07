15f09045ab198482dd95564aab906bed07ddf291
sawanya000 <sawanya000@gmail.com>
2021-04-07 10:46:56 +0700

    add get ip

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const requestIp = require("request-ip");
 
+app.use(requestIp.mw());
 
-app.get("/", (req, res)=>{
-    res.json({result: "ok", data:[1,2,3,4,5]})
-})
+app.use("/ip", function (req, res) {
+  const ip = req.clientIp;
+  res.end(ip);
+});
 
-app.get("/api" , (req, res)=>{
-    res.send("mook");
-})
+app.get("/", (req, res) => {
+  res.json({ result: "ok", data: [1, 2, 3, 4, 5] });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Serer is running. ${PORT}`)
-})
+app.get("/api", (req, res) => {
+  res.send("mook");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serer is running. ${PORT}`);
+});
